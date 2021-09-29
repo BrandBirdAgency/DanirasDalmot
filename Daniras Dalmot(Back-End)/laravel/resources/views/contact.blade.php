daa7d00409b05f995b728956588537a22a1454a2
Av-sek <abhishekghorashainee07@gmail.com>
2021-09-29 08:33:58 +0545

    Laravel Project For Danira's Dalmot
    Made a new project . Added all html to blad.php file and css to public folder.

---
+++ Daniras Dalmot(Back-End)/laravel/resources/views/contact.blade.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home | Danira's</title>
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" type="text/css" href="vendor/css/bootstrap.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="vendor/css/bootstrap.min.css"
+    />
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- FONTAWESOME -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+      integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
+    />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/contact.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+  </head>
+  <body>
+
+<!-- HEADER -->
+<header>
+  <a class="logo" href="/"
+    ><img src="./assets/images/logo.jpg" alt="logo"
+  /></a>
+  <nav>
+    <ul class="nav__links">
+      <li><a href="./index.html">Home</a></li>
+      <li><a href="./products.html">Products</a></li>
+      <li><a href="./team.html">Team</a></li>
+      <li><a href="./about.html">About</a></li>
+    </ul>
+  </nav>
+  <a class="cta" href="./contact.html">Contact</a>
+  <p class="menu cta">Menu</p>
+</header>
+<div id="mobile__menu" class="overlay">
+  <a class="close">&times;</a>
+  <div class="overlay__content">
+    <a href="./index.html">Home</a>
+    <a href="./products.html">Products</a>
+    <a href="./team.html">Team</a>
+    <a href="./about.html">About</a>
+    <a href="./contact.html">Contact</a>
+  </div>
+</div>
+<!-- END HEADER -->
+
+  <!-- MAIN BANNER -->
+  <div class="teambanner">
+    <div class="contain">
+      <div class="image">
+        <img src="./assets/images/bg.jpg" alt="" />
+      </div>
+      <div class="image-overlay"></div>
+      <div class="text">
+        <div class="headings">
+          <h1 class="">CONTACT</h1>
+          <div class="bannerline"></div>
+        </div>
+        <div class="bannernav">
+          <li><i class="fas fa-home"></i></li>
+          <li><a href="./index.html">HOME</a></li>
+          <li><i class="fas fa-caret-right"></i></li>
+          <li><a href="./about.html">CONTACT</a></li>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END MAIN BANNER -->
+
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-10">
+            <div class="wrapper">
+              <div class="row no-gutters">
+                <div class="col-md-6">
+                  <div class="contact-wrap w-100 p-lg-5 p-4">
+                    <h3 class="mb-4">Send us a message</h3>
+                    <div id="form-message-warning" class="mb-4"></div>
+                    <div id="form-message-success" class="mb-4">
+                      Your message was sent, thank you!
+                    </div>
+                    <form
+                      method="POST"
+                      id="contactForm"
+                      name="contactForm"
+                      class="contactForm"
+                    >
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input
+                              type="text"
+                              class="form-control"
+                              name="name"
+                              id="name"
+                              placeholder="Name"
+                            />
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input
+                              type="email"
+                              class="form-control"
+                              name="email"
+                              id="email"
+                              placeholder="Email"
+                            />
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input
+                              type="text"
+                              class="form-control"
+                              name="subject"
+                              id="subject"
+                              placeholder="Subject"
+                            />
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <textarea
+                              name="message"
+                              class="form-control"
+                              id="message"
+                              cols="30"
+                              rows="6"
+                              placeholder="Message"
+                            ></textarea>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input
+                              type="submit"
+                              value="Send Message"
+                              class="submit-btn"
+                            />
+                            <div class="submitting"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-md-6 d-flex align-items-stretch">
+                  <div class="info-wrap w-100 p-lg-5 p-4 img">
+                    <h3>Contact us</h3>
+                    <p class="mb-4">
+                      We're open for any suggestion or just to have a chat
+                    </p>
+                    <div class="dbox w-100 d-flex align-items-start">
+                      <div
+                        class="
+                          icon
+                          d-flex
+                          align-items-center
+                          justify-content-center
+                        "
+                      >
+                        <span class="fa fa-map-marker"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p><span>Address:</span> Parwanipur-5, Bara, Nepal</p>
+                      </div>
+                    </div>
+                    <div class="dbox w-100 d-flex align-items-center">
+                      <div
+                        class="
+                          icon
+                          d-flex
+                          align-items-center
+                          justify-content-center
+                        "
+                      >
+                        <span class="fa fa-phone"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p>
+                          <span>Phone:</span>
+                          <a href="tel://9845999137">+977 9845999137</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div class="dbox w-100 d-flex align-items-center">
+                      <div
+                        class="
+                          icon
+                          d-flex
+                          align-items-center
+                          justify-content-center
+                        "
+                      >
+                        <span class="fa fa-paper-plane"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p>
+                          <span>Email:</span>
+                          <a href="mailto:infodanirasdalmoth@gmail.com"
+                            >infodanirasdalmoth@gmail.com</a
+                          >
+                        </p>
+                      </div>
+                    </div>
+                    <div class="dbox w-100 d-flex align-items-center">
+                      <div
+                        class="
+                          icon
+                          d-flex
+                          align-items-center
+                          justify-content-center
+                        "
+                      >
+                        <span class="fa fa-globe"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p>
+                          <span>Website</span>
+                          <a href="www.danirasdalmoth.com"
+                            >danirasdalmoth.com</a
+                          >
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="google-maps">
+      <iframe
+        width="100%"
+        height="500"
+        style="border: 0"
+        loading="lazy"
+        allowfullscreen
+        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJz7lIcjdVkzkRhj2x77WzDQk&key=AIzaSyB9rtq1F5EacmlMgt_1QuNsl9d0FEZ06DY"
+      ></iframe>
+    </div>
+
+    <!-- FOOTER -->
+
+    <div class="footer-basic">
+      <footer>
+        <div class="social">
+          <a href="#"><i class="icon ion-social-instagram"></i></a
+          ><a href="#"><i class="icon ion-social-snapchat"></i></a
+          ><a href="#"><i class="icon ion-social-twitter"></i></a
+          ><a href="#"><i class="icon ion-social-facebook"></i></a>
+        </div>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Home</a></li>
+          <li class="list-inline-item"><a href="#">Products</a></li>
+          <li class="list-inline-item"><a href="#">Team</a></li>
+          <li class="list-inline-item"><a href="#">About</a></li>
+          <li class="list-inline-item"><a href="#">Contact</a></li>
+        </ul>
+        <p class="copyright">Company Name © 2018</p>
+      </footer>
+    </div>
+
+    <!-- FOOTER END -->
+
+    <!-- Script Source Files -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="vendor/js/jquery-3.6.0.min.js"></script>
+    <script src="./assets/js/main.js"></script>
+    <script src="./vendor/js/aos.js"></script>
+    <script src="vendor/js/jquery.waypoints.js"></script>
+    <script src="vendor/js/jquery.counterup.js"></script>
+    <script src="vendor/js/bootstrap.min.js"></script>
+    <script src="vendor/js/bootstrap.bundle.js"></script>
+    <script src="vendor/js/popper.min.js"></script>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/contact.js"></script>
+  </body>
+</html>
---
+++ Daniras Dalmot(Back-End)/laravel/resources/views/product.blade.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home | Danira's</title>
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" type="text/css" href="vendor/css/bootstrap.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="vendor/css/bootstrap.min.css"
+    />
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Swiper Js -->
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
+    />
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- FONTAWESOME -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+      integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
+    />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/style.css" />
+  </head>
+  <body>
+    <!-- HEADER -->
+    <header>
+      <a class="logo" href="/"
+        ><img src="./assets/images/logo.jpg" alt="logo"
+      /></a>
+      <nav>
+        <ul class="nav__links">
+          <li><a href="./index.html">Home</a></li>
+          <li><a href="./products.html">Products</a></li>
+          <li><a href="./team.html">Team</a></li>
+          <li><a href="./about.html">About</a></li>
+        </ul>
+      </nav>
+      <a class="cta" href="./contact.html">Contact</a>
+      <p class="menu cta">Menu</p>
+    </header>
+    <div id="mobile__menu" class="overlay">
+      <a class="close">&times;</a>
+      <div class="overlay__content">
+        <a href="./index.html">Home</a>
+        <a href="./products.html">Products</a>
+        <a href="./team.html">Team</a>
+        <a href="./about.html">About</a>
+        <a href="./contact.html">Contact</a>
+      </div>
+    </div>
+    <!-- END HEADER -->
+
+    <!-- MAIN BANNER -->
+
+  <!-- MAIN BANNER -->
+  <div class="teambanner">
+    <div class="contain">
+      <div class="image">
+        <img src="./assets/images/bg.jpg" alt="" />
+      </div>
+      <div class="image-overlay"></div>
+      <div class="text">
+        <div class="headings">
+          <h1 class="">PRODUCTS</h1>
+          <div class="bannerline"></div>
+        </div>
+        <div class="bannernav">
+          <li><i class="fas fa-home"></i></li>
+          <li><a href="./index.html">HOME</a></li>
+          <li><i class="fas fa-caret-right"></i></li>
+          <li><a href="./about.html">PRODCUTS</a></li>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END MAIN BANNER -->
+
+    <!-- PRODUCT DETAILS -->
+
+    <section id="display-product">
+      <div class="product-main">
+        <div class="product-image" data-tilt>
+          <img src="./assets/images/0.jpg" alt="" />
+        </div>
+        <div class="product-details">
+          <div class="product-name">
+            <p>Mixture Dalmot (In Stock)</p>
+            <div class="status"></div>
+          </div>
+          <div class="product-rating">
+            <span><i class="fas fa-star"></i></span>
+            <span><i class="fas fa-star"></i></span>
+            <span><i class="fas fa-star"></i></span>
+            <span><i class="fas fa-star"></i></span>
+            <p>(5 reviews)</p>
+          </div>
+          <div class="product-info">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+              tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Magni, adipisci!
+            </p>
+          </div>
+          <div class="product-count">
+            <p>Quantity</p>
+            <div class="counter">
+              <span class="minus"><i class="fas fa-minus"></i></span>
+              <p class="count">1</p>
+              <span class="plus"><i class="fas fa-plus"></i></span>
+            </div>
+          </div>
+          <button data-toggle="modal" data-target="#myModal">Buy</button>
+          <div class="line"></div>
+          <div class="share-product">
+            <p>Share on</p>
+            <div class="icons">
+              <span><i class="fab fa-facebook-f"></i></span>
+              <span><i class="fab fa-instagram"></i></span>
+              <span><i class="fab fa-twitter"></i></span>
+            </div>
+          </div>
+        </div>
+
+        <!-- The Modal -->
+        <div class="modal" id="myModal">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <!-- Modal Header -->
+              <div class="modal-header">
+                <h4 class="modal-title">Delivery Information</h4>
+                <button type="button" class="close" data-dismiss="modal">
+                  &times;
+                </button>
+              </div>
+
+              <!-- Modal body -->
+              <div class="modal-body">
+                <form action="">
+                  <div class="form-group">
+                    <label for="usr">Full Name:</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="usr"
+                      name="username"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="Address">Address:</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="Address"
+                      name="Address"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="Phone">Phone Number:</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="Phone"
+                      name="Phone"
+                    />
+                  </div>
+                  <button type="submit" class="submit-btn">Proceed</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- More Products -->
+
+    <section id="more-products">
+      <div class="main">
+        <div class="title">
+          <p>More Products</p>
+          <div class="line"></div>
+        </div>
+        <!-- Slider main container -->
+        <div class="swiper images">
+          <!-- Additional required wrapper -->
+          <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+              <div class="image">
+                <img src="./assets/images/0.jpg" alt="" class="slider-image"/>
+                <div class="overlay-image">
+                  <button class="view">View</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image">
+                <img src="./assets/images/1.jpg" alt="" class="slider-image"/>
+                <div class="overlay-image">
+                  <button class="view">View</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image">
+                <img src="./assets/images/2.jpg" alt="" class="slider-image"/>
+                <div class="overlay-image">
+                  <button class="view">View</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image">
+                <img src="./assets/images/3.jpg" alt="" class="slider-image"/>
+                <div class="overlay-image">
+                  <button class="view">View</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image">
+                <img src="./assets/images/4.jpg" alt="" class="slider-image"/>
+                <div class="overlay-image">
+                  <button class="view">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- If we need pagination -->
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+    </section>
+
+    <!-- FOOTER -->
+
+    <div class="footer-basic">
+      <footer>
+        <div class="social">
+          <a href="#"><i class="icon ion-social-instagram"></i></a
+          ><a href="#"><i class="icon ion-social-snapchat"></i></a
+          ><a href="#"><i class="icon ion-social-twitter"></i></a
+          ><a href="#"><i class="icon ion-social-facebook"></i></a>
+        </div>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Home</a></li>
+          <li class="list-inline-item"><a href="#">Products</a></li>
+          <li class="list-inline-item"><a href="#">Team</a></li>
+          <li class="list-inline-item"><a href="#">About</a></li>
+          <li class="list-inline-item"><a href="#">Contact</a></li>
+        </ul>
+        <p class="copyright">Company Name © 2018</p>
+      </footer>
+    </div>
+
+    <!-- FOOTER END -->
+
+    <!-- Script Source Files -->
+    <script src="vendor/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="vendor/js/jquery-3.6.0.min.js"></script>
+    <script src="./assets/js/main.js"></script>
+    <script src="./vendor/js/aos.js"></script>
+    <script src="vendor/js/jquery.waypoints.js"></script>
+    <script src="vendor/js/jquery.counterup.js"></script>
+    <script src="vendor/js/bootstrap.bundle.js"></script>
+    <script src="vendor/js/popper.min.js"></script>
+    <script src="assets/js/tilt.jquery.js"></script>
+  </body>
+</html>
