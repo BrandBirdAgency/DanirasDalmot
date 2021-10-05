3743be72244d1ba20bbfd1baf4a3f705d445c45e
Aryaman Shrestha <aryaman.xrestha@gmail.com>
2021-10-05 23:06:28 +0545

    Migration table created

---
+++ Daniras Dalmot/sql/addproduct.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2021 at 07:18 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `danirasdalmot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addproduct`
+--
+
+CREATE TABLE `addproduct` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `p_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `b_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `r_price` double(8,2) NOT NULL,
+  `discount` double(8,2) NOT NULL,
+  `price` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addproduct`
+--
+ALTER TABLE `addproduct`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addproduct`
+--
+ALTER TABLE `addproduct`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
