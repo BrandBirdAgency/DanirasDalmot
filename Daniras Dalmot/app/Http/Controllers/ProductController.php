e891e57dd81dcdf28807fe4779b697f071317dac
Av-sek <abhishekghorashainee07@gmail.com>
2021-10-07 22:09:33 +0545

    Google Captcha
    Google Captcha ko comment hatayo

--- Daniras Dalmot/app/Http/Controllers/ProductController.php
+++ Daniras Dalmot/app/Http/Controllers/ProductController.php
@@ -111,9 +111,6 @@ class ProductController extends Controller
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
