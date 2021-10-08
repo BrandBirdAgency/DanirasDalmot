c6e02d9a55836b491dc610f724680785772e5593
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-10-08 20:26:16 +0545

    session msg fix

--- Daniras Dalmot/resources/views/team.blade.php
+++ Daniras Dalmot/resources/views/team.blade.php
@@ -15,25 +15,17 @@
 
     <!-- MAIN BANNER -->
     <div class="teambanner">
-        <div class="container">
-            @if (Session::has('success'))
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {!! Session::get('success') !!}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
-            @if (Session::has('error'))
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    {!! Session::get('error') !!}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
-        </div>
         <div class="contain">
+            <div class="container">
+                @if (Session::has('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {!! Session::get('success') !!}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
+            </div>
           <div class="image">
             <img src="./assets/images/bg.jpg" alt="" />
           </div>
