ed89bb2452e7e26f78559798ea750dc5f5602ff6
Shubha <shubhadon7@gmail.com>
2021-10-09 08:13:38 +0545

    Add form updated and some useless cdn removed

--- Daniras Dalmot/resources/views/inc/script.blade.php
+++ Daniras Dalmot/resources/views/inc/script.blade.php
@@ -4,9 +4,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="vendor/js/jquery-3.6.0.min.js"></script>
 <script src="./assets/js/main.js"></script>
-<script src="./vendor/js/aos.js"></script>
-<script src="vendor/js/jquery.waypoints.js"></script>
-<script src="vendor/js/jquery.counterup.js"></script>
+
 <script src="vendor/js/bootstrap.bundle.js"></script>
 <script src="vendor/js/popper.min.js"></script>
 <script src="assets/js/tilt.jquery.js"></script>
