0815c19001e5719c16bdb50b8d5266296d402672
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-10-09 09:10:11 +0545

    Fixes

--- Daniras Dalmot/app/Http/Controllers/AdminController.php
+++ Daniras Dalmot/app/Http/Controllers/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\About;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
--- Daniras Dalmot/resources/views/admin/dashboard.blade.php
+++ Daniras Dalmot/resources/views/admin/dashboard.blade.php
@@ -43,6 +43,5 @@
 @endsection
 
 @section('js')
-    <script src="script.js"></script>
     <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
 @endsection
