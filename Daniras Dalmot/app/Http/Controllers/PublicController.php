c25a3f30f693b81462c9bcb5c654eae4d45e7344
Av-sek <abhishekghorashainee07@gmail.com>
2021-10-10 12:15:15 +0545

    Hot Product
    Hot Products display in product page and routing to buy product

--- Daniras Dalmot/app/Http/Controllers/PublicController.php
+++ Daniras Dalmot/app/Http/Controllers/PublicController.php
@@ -12,8 +12,17 @@ class PublicController extends Controller
 {
     public function index()
     {
+        $hot= array();
+        $products = Product::all();
         $about = About::first();
-        return view('index', compact('about'));
+        foreach($products as $pro)
+        {
+            if($pro->hot)
+            {
+                $hot[] = $pro;
+            }
+        }
+        return view('index', compact('about','hot'));
     }
 
     public function about()
