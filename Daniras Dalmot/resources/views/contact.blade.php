fab2091bfc61c7845727dda79d016bbcfb4a4cae
Sagar Chhetri <slaure354@gmail.com>
2021-10-21 22:21:17 +0545

    map update

--- Daniras Dalmot/resources/views/contact.blade.php
+++ Daniras Dalmot/resources/views/contact.blade.php
@@ -261,8 +261,9 @@
 </section>
 
 <div class="google-maps">
-  <iframe width="100%" height="500" style="border: 0" loading="lazy" allowfullscreen
-    src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJz7lIcjdVkzkRhj2x77WzDQk&key=AIzaSyB9rtq1F5EacmlMgt_1QuNsl9d0FEZ06DY"></iframe>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3553.8693240452435!2d84.90640731485557!3d27.034294883075386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3993554b072649b3%3A0xb7fdfeee98289715!2sDanira&#39;s%20Dalmoth!5e0!3m2!1sen!2snp!4v1634834105799!5m2!1sen!2snp"
+    width="100%" height="500" style="border: 0" allowfullscreen loading="lazy"></iframe>
 </div>
 @endsection
 @section('js')
