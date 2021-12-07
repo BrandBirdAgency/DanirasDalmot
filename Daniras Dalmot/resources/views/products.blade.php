55d68b505ca4e5704bc548dc520260bdfc9940fc
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-12-07 08:34:53 +0545

    Update products.blade.php

--- Daniras Dalmot/resources/views/products.blade.php
+++ Daniras Dalmot/resources/views/products.blade.php
@@ -97,7 +97,7 @@
                                             href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdanirasdalmoth.com%2Fproducts%2F{{ $p->id }}&amp;src=sdkpreparse"
                                             class="fb-xfbml-parse-ignore">Share</a></div>
                                     <a target="_blank" href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-                                        class="twitter-share-button" data-text="{{ $p->description }}"
+                                        class="twitter-share-button"
                                         data-url="https://danirasdalmoth.com/products/{{ $p->id }}"
                                         data-hashtags="danirasdalmoth" data-show-count="false">Tweet</a>
                                     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
