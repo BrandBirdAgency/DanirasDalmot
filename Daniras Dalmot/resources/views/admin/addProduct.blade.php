4750eb8973e6b9173fa95076100d4baf49a1a7a9
Sagar Chhetri <slaure354@gmail.com>
2021-12-08 08:51:12 +0545

    Front End Fixes

--- Daniras Dalmot/resources/views/admin/addProduct.blade.php
+++ Daniras Dalmot/resources/views/admin/addProduct.blade.php
@@ -21,249 +21,140 @@
     <form class="card-body add-product-body" method="POST" action={{route('product.store')}}
       enctype="multipart/form-data">
       @csrf
+      @csrf
       <div class="row">
         <div class="col-md-4 col-12">
           <div class="form-group">
             <label for="inp" class=""> Product Name </label>
-
-            <input type="text" id="inp" class="form-control form-control-sm" name="name" required />
+            <input type="text" id="inp" class="form-control form-control-sm" name="name" value="{{ old('name')}}" />
             @error('name')
             {{$message}}
             @enderror
           </div>
-          <form class="card-body add-product-body" method="POST" action={{route('product.store')}}
-            enctype="multipart/form-data">
-            @csrf
-            <div class="row">
-              <div class="col-md-4 col-12">
-                <div class="form-group">
-                  <label for="inp" class=""> Product Name </label>
 
-                  <input type="text" id="inp" class="form-control form-control-sm" name="name"
-                    value="{{ old('name')}}" />
-                  @error('name')
-                  {{$message}}
-                  @enderror
-                </div>
+          <div class="form-group">
+            <label for="inouttextarea"> Product Description</label>
+            <textarea class="form-control" id="inputtextarea" rows="3" name="description"
+              value="{{ old('description')}}"></textarea>
+            @error('description')
+            {{$message}}
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="inouttextarea">Category</label>
+            <select id="inp" class="form-control form-control-sm" name="category" value="{{ old('category')}}">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="inouttextarea">Size</label>
+            <input type="text" id="inp" class="form-control form-control-sm" name="size" value="{{ old('size')}}" />
+            @error('size')
+            {{$message}}
+            @enderror
+          </div>
+        </div>
+        <div class="col-md-4 col-12">
+          <div class="form-group">
+            <label for="inp" class="">Brand Name</label>
+            <input type="text" id="inp" class="form-control form-control-sm" name="brand_name"
+              value="{{ old('brand_name')}}" />
+            @error('brand_name')
+            {{$message}}
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="inp" class="">Retail Price</label>
 
+            <input type="number" id="inp" class="form-control form-control-sm" name="retail_price"
+              value="{{ old('retail_price')}}" />
+            @error('retail_price')
+            {{$message}}
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="inouttextarea">Price</label>
+            <input type="number" id="inp" class="form-control form-control-sm" name="price" value="{{ old('price')}}" />
+            @error('price')
+            {{$message}}
+            @enderror
+          </div>
 
-                <div class="form-group">
-                  <label for="inouttextarea"> Product Description</label>
-                  <textarea class="form-control" id="inputtextarea" rows="3" name="description"
-                    value="{{ old('description')}}"></textarea>
-                  @error('description')
-                  {{$message}}
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="inouttextarea">Category</label>
-                  <select id="inp" class="form-control form-control-sm" name="category" value="{{ old('category')}}">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
+          <div class="form-group">
+            <label for="inouttextarea">Discount</label>
+            <input type="number" id="inp" class="form-control form-control-sm" name="discount"
+              value="{{ old('discount')}}" />
+            @error('discount')
+            {{$message}}
+            @enderror
+          </div>
+        </div>
 
-                  </select>
-                </div>
-                <div class="form-group">
-                  <label for="inouttextarea">Size</label>
-                  <input type="text" id="inp" class="form-control form-control-sm" name="size"
-                    value="{{ old('size')}}" required/>
-                  @error('size')
-                  {{$message}}
-                  @enderror
-                </div>
-
-
-              </div>
-              <div class="col-md-4 col-12">
-                <div class="form-group">
-                  <label for="inp" class="">Brand Name</label>
-
-                  <input type="text" id="inp" class="form-control form-control-sm" name="brand_name"
-                    value="{{ old('brand_name')}}" required/>
-                  @error('brand_name')
-                  {{$message}}
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="inp" class="">Retail Price</label>
-
-                  <input type="number" id="inp" class="form-control form-control-sm" name="retail_price"
-                    value="{{ old('retail_price')}}" required/>
-                  @error('retail_price')
-                  {{$message}}
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="inouttextarea">Price</label>
-                  <input type="number" id="inp" class="form-control form-control-sm" name="price"
-                    value="{{ old('price')}}" required/>
-                  @error('price')
-                  {{$message}}
-                  @enderror
-                </div>
-
-                <div class="form-group">
-                  <label for="inouttextarea">Discount</label>
-                  <input type="number" id="inp" class="form-control form-control-sm" name="discount"
-                    value="{{ old('discount')}}" required/>
-                  @error('discount')
-                  {{$message}}
-                  @enderror
-                </div>
-
-              </div>
-              <div class="col-md-4 col-12">
-                <div class="form-group">
-                  <label for="inp" class="">Brand Name</label>
-
-                  <input type="text" id="inp" class="form-control form-control-sm" name="brand_name" required />
-                  @error('brand_name')
-                  {{$message}}
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="inp" class="">Retail Price</label>
-
-                  <input type="number" id="inp" class="form-control form-control-sm" name="retail_price" required />
-                  @error('retail_price')
-                  {{$message}}
-                  @enderror
-                </div>
-                <div class="form-group">
-                  <label for="inouttextarea">Price</label>
-                  <input type="number" id="inp" class="form-control form-control-sm" name="price" required />
-                  @error('price')
-                  {{$message}}
-                  @enderror
-                </div>
-
-                <<<<<<< HEAD <div class="form-group">
-                  <label for="inouttextarea">Discount</label>
-                  <input type="number" id="inp" class="form-control form-control-sm" name="discount" required />
-                  @error('discount')
-                  {{$message}}
-                  @enderror
-              </div>
-
+        <div class="col-md-4 col-12 text-left">
+          <div class="form-group">
+            <label for="inputtextarea">Bar Number (Optional)</label>
+            <input type="number" id="inp" class="form-control form-control-sm" name="b_num" />
+            @error('price')
+            {{$message}}
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="inputfile" class="">Product Image </label>
+            <div class="upload">
+              <input type="file" name='photo' value="{{ old('photo')}}" accept="image/*" id="real-file0"
+                hidden="hidden" />
+              <button type="button" id="custom-button0" class="btn">
+                Choose an image
+              </button>
+              <p id="custom-text0">No file chosen, yet.</p>
             </div>
+            @error('image')
+            {{$message}}
+            @enderror
+          </div>
 
+          <div class="form-group">
+            <label for="inputfile" class="">Qr Code </label>
+            <div class="upload">
+              <input type="file" name="qr" value="{{ old('qr')}}" id="real-file2" hidden="hidden" />
+              <button type="button" id="custom-button2" class="btn">
+                Choose an image
+              </button>
+              <p id="custom-text2">No file chosen, yet.</p>
+            </div>
+          </div>
 
-            <div class="col-md-4 col-12 text-left">
-              <div class="form-group">
-                <label for="inputtextarea">Bar Number (Optional)</label>
-                <input type="number" id="inp" class="form-control form-control-sm" name="b_num" />
-                @error('price')
-                {{$message}}
-                @enderror
-              </div>
-              =======
-              <div class="col-md-4 col-12 text-left">
-                <div class="form-group">
-                  <label for="inputtextarea">Bar Number (Optional)</label>
-                  <input type="number" id="inp" class="form-control form-control-sm" name="b_num"
-                    value="{{ old('b_num')}}" />
-                  @error('price')
-                  {{$message}}
-                  @enderror
-                </div>
+          <div class="form-group">
+            <label for="inputfile" class="">Bar Code </label>
+            <div class="upload">
+              <input type="file" name="bar" value="{{ old('bar')}}" id="real-file1" hidden="hidden" />
+              <button type="button" id="custom-button1" class="btn">
+                Choose an image
+              </button>
+              <p id="custom-text1">No file chosen, yet.</p>
+            </div>
+          </div>
 
-                <div class="form-group">
-                  <label for="inputfile" class="">Product Image </label>
-                  <div class="upload">
-                    <input type="file" name='photo' value="{{ old('photo')}}" accept="image/*" id="real-file0"
-                      hidden="hidden" />
-                    <button type="button" id="custom-button0" class="btn">
-                      Choose an image
-                    </button>
-                    <p id="custom-text0">No file chosen, yet.</p>
-                  </div>
-                  @error('image')
-                  {{$message}}
-                  @enderror
-                </div>
-
-                <div class="form-group">
-                  <label for="inputfile" class="">Qr Code </label>
-                  <div class="upload">
-                    <input type="file" name="qr" value="{{ old('qr')}}" id="real-file2" hidden="hidden" />
-                    <button type="button" id="custom-button2" class="btn">
-                      Choose an image
-                    </button>
-                    <p id="custom-text2">No file chosen, yet.</p>
-                  </div>
-                </div>
-
-                <div class="form-group">
-                  <label for="inputfile" class="">Bar Code </label>
-                  <div class="upload">
-                    <input type="file" name="bar" value="{{ old('bar')}}" id="real-file1" hidden="hidden" />
-                    <button type="button" id="custom-button1" class="btn">
-                      Choose an image
-                    </button>
-                    <p id="custom-text1">No file chosen, yet.</p>
-                    >>>>>>> ef79fb2110aaa5e7eb0209ac4f64f10466036ba7
-
-                    <div class="form-group">
-                      <label for="inputfile" class="">Product Image </label>
-                      <div class="upload">
-                        <input type="file" name='photo' accept="image/*" id="real-file0" hidden="hidden" required />
-                        <button type="button" id="custom-button0" class="btn">
-                          Choose an image
-                        </button>
-                        <p id="custom-text0">No file chosen, yet.</p>
-                      </div>
-                      @error('image')
-                      {{$message}}
-                      @enderror
-                    </div>
-
-                    <div class="form-group">
-                      <label for="inputfile" class="">Qr Code </label>
-                      <div class="upload">
-                        <input type="file" name="qr" id="real-file2" hidden="hidden" />
-                        <button type="button" id="custom-button2" class="btn">
-                          Choose an image
-                        </button>
-                        <p id="custom-text2">No file chosen, yet.</p>
-                      </div>
-                    </div>
-
-                    <div class="form-group">
-                      <label for="inputfile" class="">Bar Code </label>
-                      <div class="upload">
-                        <input type="file" name="bar" id="real-file1" hidden="hidden" />
-                        <button type="button" id="custom-button1" class="btn">
-                          Choose an image
-                        </button>
-                        <p id="custom-text1">No file chosen, yet.</p>
-
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-
-                <div class="submitbtn">
-                  <button type="submit" class="btn upload">
-                    <small> Add</small>
-                  </button>
-                </div>
-          </form>
         </div>
       </div>
-      @endsection
-      @section('js')
 
-
-      <!-- Add product -end-->
-
-      <!-- Script Source Files -->
-      <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
-
-      <script>
-        const realFileBtn0 = document.getElementById("real-file0");
+      <div class="submitbtn">
+        <button type="submit" class="btn upload">
+          <small> Add</small>
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
+@endsection
+@section('js')
+<!-- Script Source Files -->
+<script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
+<script>
+  const realFileBtn0 = document.getElementById("real-file0");
       const customBtn0 = document.getElementById("custom-button0");
       const customTxt0 = document.getElementById("custom-text0");
 
@@ -315,5 +206,5 @@
           customTxt2.innerHTML = "No file choosen, yet.";
         }
       });
-      </script>
-      @endsection
+</script>
+@endsection
